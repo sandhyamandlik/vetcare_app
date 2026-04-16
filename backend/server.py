5cc9abf879273c50c0d4391ef6497ed4f340879e
Sandhyam2106 <158207707+sandhyamandlik@users.noreply.github.com>
2026-04-16 10:33:58 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -778,7 +778,3 @@ app.add_middleware(
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=True)
